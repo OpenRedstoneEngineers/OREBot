561fca74c8f3bfa00cf1e154e63b10fced14a9b7
Omar Saad <omar_saad12@hotmail.com>
2015-07-07 17:25:06 +0200

    okay this is actually the last mistake lol sorry about the spam

--- main.py
+++ main.py
@@ -187,9 +187,9 @@ class IRCClient:
                         self.say("If you would like to apply for a server, please visit https://openredstone.org/apply/",target2)
                         self.say("Please read the signs before proceeding to ask questions",target2)
                         for c in self.services:
-                            self.say(".msg %s Welcome to ORE, %s! In order to get started, you can teleport to the welcome signs using /welcome" % (target2,target2),c)
-                            self.say(".msg %s If you would like to apply for a server, please visit https://openredstone.org/apply/" % (target2),c)
-                            self.say(".msg %s Please read the signs before proceeding to ask questions" % (target2),c)
+                            self.send("PRIVMSG %s :.msg %s Welcome to ORE, %s! In order to get started, you can teleport to the welcome signs using /welcome" % (c,target2,target2))
+                            self.send("PRIVMSG %s :.msg %s If you would like to apply for a server, please visit https://openredstone.org/apply/" % (c,target2))
+                            self.send("PRIVMSG %s :.msg %s Please read the signs before proceeding to ask questions" % (c,target2))
                 elif self.ctx["type"] == "PRIVMSG" and (self.nickname.lower() in self.ctx["msg"].lower()):
                     # something is speaking to the bot
                     query = self.ctx["msg"].lower().replace(self.nickname.lower(), "".join((self.bcolors["UNDERLINE"], self.nickname.lower(), self.bcolors["ENDC"])))
