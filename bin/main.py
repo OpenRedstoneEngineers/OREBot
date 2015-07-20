92c380151881e127daf4515f1243203f69e87328
Adam Gausmann <adam@nonemu.info>
2015-07-20 08:30:22 -0500

    Added basic raygun support

--- bin/main.py
+++ bin/main.py
@@ -2,6 +2,7 @@
 
 import json
 import os
+import sys
 
 from orebot import bot
 
@@ -10,6 +11,20 @@ if os.path.isfile("./config.json"):
         config = json.load(f)
 else:
     config = {}
+
+if "raygun_key" in config:
+    raygun_key = config.pop("raygun_key")
+else:
+    raygun_key = None
+
+if raygun_key is not None:
+    from raygun4py import raygunprovider
+
+    def handle_exception(exc_type, exc_value, exc_traceback):
+        c = raygunprovider.RaygunSender(raygun_key)
+        c.send_exception(exc_info=(exc_type, exc_value, exc_traceback))
+        print("Error has been reported to Raygun")
+    sys.excepthook = handle_exception
 
 bot = bot.OREBot(**config)
 bot.run()
